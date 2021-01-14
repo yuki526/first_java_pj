773d2c7a6c1ed3ac53c6d133d1f5da75c00e28bc
yuki526 <yk.g187@gmail.com>
2021-01-14 12:02:46 +0900

    コンストラクタ定義によるnewの記述変更

--- Java_S8/src/S8/Main.java
+++ Java_S8/src/S8/Main.java
@@ -3,8 +3,7 @@ package S8;
 public class Main {
 
 	public static void main(String[] args) {
-		Cleric c = new Cleric();
-		c.name = "聖職者";
+		Cleric c = new Cleric("ゆき");
 		c.selfAid();
 		c.pray(1);
 
