3e7da4e0f32378358ad66d51c9d34cd039cf7b6e
yuki526 <yk.g187@gmail.com>
2021-01-30 15:05:54 +0900

    投稿処理実装

--- docoTsubu/src/servlet/Main.java
+++ docoTsubu/src/servlet/Main.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 
@@ -45,4 +46,30 @@ public class Main extends HttpServlet {
 			dispatcher.forward(request, response);
 		}
 	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		if (text != null && text.length() != 0) {
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+//			つぶやきをリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.exesute(mutter, mutterList);
+
+			application.setAttribute("mutterList", mutterList);
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+
+		}
+	}
+
 }
